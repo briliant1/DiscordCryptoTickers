5cceaca1818c9cafc0b1da229d5eb8a70e539526
alteregog1 <88210725+alteregog1@users.noreply.github.com>
2021-08-24 16:23:06 +0700

    using build in discord tasks

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,11 @@
 import asyncio
 import os
-import random
+from itertools import cycle
 
+import aiohttp
 import discord
-import httpx
-from discord.ext import commands
+import random
+from discord.ext import commands, tasks
 from discord_slash import SlashCommand
 from dotenv import load_dotenv
 
@@ -16,7 +17,6 @@ main_currency_api = os.environ['MAIN_CURRENCY_NAME']
 main_currency_symbol = os.environ['MAIN_CURRENCY_SYMBOL']
 currency_to_show = os.environ['CURRENCY_TO_SHOW']
 currencies_to_watch = os.environ['VS_CURRENCIES']
-wait_duration = int(os.environ['WAIT_DURATION'])
 discord_token = os.environ['DISCORD_TOKEN']
 
 coingecko = f"https://api.coingecko.com/api/v3/simple/price?ids={main_currency_api}&vs_currencies={currencies_to_watch}"
@@ -25,47 +25,43 @@ coingecko = f"https://api.coingecko.com/api/v3/simple/price?ids={main_currency_a
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    asyncio.create_task(task_update_activity())
+    task_update_activity.start()
 
 
-def get_coingecko_data():
-    with httpx.Client(timeout=None) as httpx_client:
-        response = httpx_client.get(coingecko)
-        data = response.json()
-        return data
+async def get_coingecko_data():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(coingecko) as resp:
+            data = await resp.json()
+            return data
 
 
-def value_of_currency_to_show():
-    return float(get_coingecko_data()[main_currency_api][currency_to_show])
+async def value_of_currency_to_show():
+    return float((await get_coingecko_data())[main_currency_api][currency_to_show])
 
 
-def watch_secondary_currencies():
+async def watch_secondary_currencies():
     split_second_currency = [x for x in currencies_to_watch.split(",") if x != currency_to_show]
     currencies = []
     for second_currency in split_second_currency:
         data = {
             "symbol": second_currency,
-            "value": float(get_coingecko_data()[main_currency_api][second_currency])
+            "value": float((await get_coingecko_data())[main_currency_api][second_currency])
         }
         currencies.append(data)
     return currencies
 
 
+@tasks.loop(seconds=5.0)
 async def task_update_activity():
-    await client.wait_until_ready()
-    while not client.is_closed():
-        for guild in client.guilds:
-            await guild.me.edit(nick=f"{value_of_currency_to_show():,} {currency_to_show.upper()}/{main_currency_symbol}")
+    for guild in client.guilds:
+        await guild.me.edit(nick=f"{(await value_of_currency_to_show()):,} {currency_to_show.upper()}/{main_currency_symbol}")
 
-        status = []
-        for data in watch_secondary_currencies():
-            status_name = f"{data['value']:,} {data['symbol'].upper()}"
-            status.append(status_name)
-
-        activity_status = random.choice(status)
-
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity_status))
-        await asyncio.sleep(wait_duration)
+    status = []
+    secondary_currency = await watch_secondary_currencies()
+    for data in secondary_currency:
+        status_name = f"{data['value']:,} {data['symbol'].upper()}"
+        status.append(status_name)
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=random.choice(status)))
 
 
 client.run(discord_token)
